38c5c67a1172036c18343a4b73b8259b48742f50
Cody Patterson <copatterson@csumb.edu>
2017-06-20 17:24:20 -0700

    worked on home

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var sections = {
-    home: '#home',
-    tech: '#tech',
-    projects: '#projects',
-    social: '#social',
-    stats: '#stats',
-    resume: '#resume',
-    employment: '#employment',
-    contact: '#contact'
-};
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+var age = getAge('1991/07/11');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', sections: sections});
+    res.render('index', { title: 'Express', age: age});
 });
 
 module.exports = router;
